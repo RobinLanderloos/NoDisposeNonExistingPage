28a42629b7ea931126a096d88c72913a2835d9a9
Robin Landerloos <rlanderloos@sbssolutions.be>
2022-10-14 11:03:21 +0200

    Add project files.

---
+++ NoDisposeNonExistingPage/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.Timers;
+
+namespace NoDisposeNonExistingPage.Pages
+{
+    public partial class Index : IDisposable
+    {
+        private System.Timers.Timer _timer;
+        private double _timerInterval = 1000;
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+            _logger.LogInformation("Dispose called");
+        }
+
+        protected override void OnInitialized()
+        {
+            _timer = new System.Timers.Timer(_timerInterval);
+            _timer.Elapsed += OnElapsed;
+            _timer.Start();
+        }
+
+        private void OnElapsed(object? s, ElapsedEventArgs e)
+        {
+            _logger.LogInformation("Timer elapsed");
+        }
+    }
+}
